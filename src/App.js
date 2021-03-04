962432ef651108a22912c7c941f16a12de97bfc6
Jonas Ramos <jjonasramos@hotmail.com>
2021-03-04 01:34:23 -0300

    Qlik Connection Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useQlik } from './context/Qlik';
+
 import './App.css';
 
 function App() {
+
+  const { app, qlik } = useQlik();
+
+  useEffect(() => {
+    console.log(qlik);
+    console.log(app);
+  }, [app])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Welcome to Qlik React Mashup Template!
       </header>
     </div>
   );
---
+++ src/context/Qlik.js
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect, useContext } from 'react';
+import QlikConnection from '../services/QlikConnection';
+
+const QlikContext = createContext();
+
+export default function QlikProvider({ children }) {
+  const[qlik, setQlik] = useState(null);
+  const[app, setApp] = useState(null);
+
+  const config = {
+    host: 'localhost',
+    prefix: '/',
+    port: 4848,
+    isSecure: false
+  };
+
+  useEffect(() => {
+    QlikConnection(config).then(qlik => {
+      setQlik(qlik);
+      setApp(qlik.openApp('Treinamento - ME', config));
+    });
+  }, []);
+
+  return(
+    <QlikContext.Provider value={{
+      qlik, app
+    }}>
+      {children}
+    </QlikContext.Provider>
+  );
+}
+
+export function useQlik() {
+  const context = useContext(QlikContext);
+  const { qlik, app } = context;
+  return { qlik, app };
+}
